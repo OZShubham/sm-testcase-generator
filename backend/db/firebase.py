07af50ab75c5f39391f6eda1667a003b3f029c94
Shubham Mishra <smdell83@gmail.com>
2025-09-23 11:45:41 +0530

    initial upload

--- backend/db/firebase.py
+++ backend/db/firebase.py
@@ -5,12 +5,12 @@ from firebase_admin import credentials, firestore
 db = None
 
 try:
-    if not firebase_admin._apps:
-        # For development, you can use a service account key file
-        # Correctly locate credentials.json from the backend directory
-        dir_path = os.path.dirname(os.path.realpath(__file__))
-        cred = credentials.Certificate(os.path.join(dir_path, "..", "credentials.json"))
-        firebase_admin.initialize_app(cred)
+    # if not firebase_admin._apps:
+    #     # For development, you can use a service account key file
+    #     # Correctly locate credentials.json from the backend directory
+    #     dir_path = os.path.dirname(os.path.realpath(__file__))
+    #     cred = credentials.Certificate(os.path.join(dir_path, "..", "credentials.json"))
+    #     firebase_admin.initialize_app(cred)
     
     # Initialize Firestore
     db = firestore.client()
